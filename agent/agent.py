41282573f044cf1e57e47d3abd443aeac2d29102
Lucas Emanuel Rocha Martins <lucas.martins@niceplanet.geo>
2026-02-18 13:47:26 -0300

    Add new classes

--- agent/agent.py
+++ agent/agent.py
@@ -4,9 +4,27 @@ from langchain_openai import ChatOpenAI
 from pydantic import BaseModel
 from langgraph.graph import StateGraph
 from langgraph.graph import START
-from langgraph.graph import END
+from langgraph.graph import END as END_STATE
 from langgraph.graph import State
+from typing import List
+from datetime import datetime
+from langchain.schema import AIMessage
+
+class Interleaving(BaseModel):
+    method: str = Field(description="Método de Interleaving")
+    messages: List[AIMessage] = Field(description="Mensagens para enviar ao usuário")
+    days_after: int = Field(description="Dias após o envio da mensagem")
+    type_interleaving: str = Field(description="Tipo de Interleaving: Procedural ou ")
+
+class StudyRecuperation(BaseModel):
+    interleavings: List[Interleaving] = Field(description="Lista de Interleavings")
+    date_spacings: List[datetime] = Field(description="Datas de espaçamento")
+
+class State(StateGraph):
+    study_recuperation: StudyRecuperation = Field(description="Recuperação de estudo")
+    messages: List[Message] = Field(description="Mensagens para enviar ao usuário")
 
 
-class ChooseInterleaving(BaseModel):
-    
+
+builder = StateGraph(State)
+builder.add_node(START, State())
